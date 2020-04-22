eae69180ce00a20b0736fe1bd28c0a7f10bbd3ed
hixanny <34408263+hixanny@users.noreply.github.com>
2020-04-22 21:36:10 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -2,8 +2,8 @@
 #=================================================
 # Description: DIY script
 # Lisence: MIT
-# Author: P3TERX
+# Author: Hixanny
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.123.253/192.168.50.5/g' package/base-files/files/bin/config_generate
