3b17ede2ef752cc4370a518ce1b1ad15f5ec2cbe
hixanny <34408263+hixanny@users.noreply.github.com>
2020-04-23 12:42:43 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -2,11 +2,11 @@
 #=================================================
 # Description: DIY script
 # Lisence: MIT
- Author: Hixanny
+# Author: Hixanny
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
 sed -i 's/192.168.123.253/192.168.50.5/g' package/base-files/files/bin/config_generate
-get clone https://github.com/Lienol/openwrt-package
+src-git lienol https://github.com/Lienol/openwrt-package
 ./scripts/feeds update -a
 ./scripts/feeds install -a
